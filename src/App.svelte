3e9cb79e8bb5f11043eaaf59ddcbbaef22e412aa
John Brimley <johnbrimley@gmail.com>
2025-12-29 17:08:02 -0700

    fixed name

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
 
   onMount(async () => {
     try {
-      const response = await fetch(`${import.meta.env.BASE_URL}data.json`)
+      const response = await fetch(`${import.meta.env.BASE_URL}rawRows.json`)
       if (!response.ok) {
         throw new Error(`Failed to load data (${response.status})`)
       }
